1de8ebfb5fb8f87e3b5c45c32f3a7ee515748aa4
QWARDZ <johnedwardbearnezaa@gmail.com>
2025-03-18 14:27:07 +0800

    New Version

--- admin/index.php
+++ admin/index.php
@@ -70,6 +70,7 @@ closeDB($conn);
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
     <!-- Custom CSS -->
     <link href="../assets/css/style.css" rel="stylesheet">
+    <link href="../assets/css/admin/sidebar.css" rel="stylesheet">
     <!-- Font Awesome -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
     <link href="../assets/css/admin/sidebar.css" rel="stylesheet">
